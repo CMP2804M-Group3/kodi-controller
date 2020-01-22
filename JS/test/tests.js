60aa7f13939ff85c576473d14eb80a55efc48b3d
Harry <harrysmod69@gmail.com>
2020-01-22 17:45:24 +0000

    add test test

---
+++ JS/test/tests.js
@@ -0,0 +1,26 @@
+var assert = require("assert"),
+	sinon = require("nock"),
+	Controller = require("controller");
+
+const RPCVersion = "2.0";
+
+describe("controller", () => {
+	describe("getActivePlayerID", () => {
+		it("should return the id of the player", (done) => {
+			var c = new Controller();
+
+			nock("http://localhost:8080")
+			.post("/jsonrpc", {
+				jsonrpc: RPCVersion,
+				method: "Player.GetActivePlayers"
+			})
+			.reply(200, `{"id":1,"jsonrpc":"2.0","result":[{"playerid":1337,"playertype":"internal","type":"video"}]}`);
+
+			c.getActivePlayerID((err, playerID) => {
+				if (err) done(err);
+				assert.equal(1337, playerID);
+				done();
+			});
+		});
+	});
+});
